27bc5a2f68bc6a3b9cfda04b44003980e79b70ac
Md Firoj Kabir <firoj.kabir65@gmail.com>
2020-11-21 12:39:05 +0100

    JSX to JavaScript

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
 function MainContent() {
+    const firstName = "Ahmed"
+    const lastName = "Rimon"
+
     return(
         <main className="content">
-            <h1>Mr. Kabir</h1>
+            <h1>Hello, {firstName + " " + lastName}</h1>
+            {/* <h1>Hello, {`${firstName} ${lastName}`}</h1> */}
             <p>I'm a single nice dev guy. Looking for internship for my master's degree.</p>
             <ol>
                 <li>Salzburg</li>
