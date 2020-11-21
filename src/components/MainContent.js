3dfe4d702527dcfc0258198b1b5549dc7280dba6
Md Firoj Kabir <firoj.kabir65@gmail.com>
2020-11-21 13:04:49 +0100

    JSX to JavaScript (Day of time)

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -4,6 +4,18 @@ function MainContent() {
     const firstName = "Ahmed"
     const lastName = "Rimon"
 
+    const date = new Date()
+    const hours = date.getHours()
+    let timeOfDay
+
+    if (hours < 12) {
+        timeOfDay = "morning"
+    } else if (hours >= 12 && hours < 17) {
+        timeOfDay = "afternoon"
+    } else {
+        timeOfDay = "night"
+    }
+
     return(
         <main className="content">
             <h1>Hello, {firstName + " " + lastName}</h1>
@@ -14,6 +26,8 @@ function MainContent() {
                 <li>Santorini</li>
                 <li>Ibiza</li>
             </ol>
+
+            <h2>Good {timeOfDay}</h2>
         </main>
     )
 }
