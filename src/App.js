931dc7db440258e7be30f6b8448b78b7b9855336
Md Firoj Kabir <firoj.kabir65@gmail.com>
2020-12-01 21:44:52 +0100

    Use of props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React from 'react'
 
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import ToDoApp from './components/ToDoApp'
+import ContactCard from './components/ContactCard'
 
 import './App.css';
 
@@ -13,7 +14,18 @@ function App() {
             {/* <Header />
             <MainContent />
             <Footer /> */}
-            <ToDoApp />
+            {/* <ToDoApp /> */}
+            <ContactCard 
+                contact={{name:"Mr. Whiskerson", imgUrl:"http://placekitten.com/300/200", phone:"(212) 555-1234", email:"mr.whiskaz@catnap.meow" }}
+            />
+            <ContactCard 
+                contact={{name:"Fluffykins", imgUrl:"http://placekitten.com/400/200", phone:"(212) 555-2345", email:"fluff@me.com" }}
+                
+            />
+            <ContactCard 
+                contact={{name:"Destroyer", imgUrl:"http://placekitten.com/200/300", phone:"(212) 555-4567", email:"ofworlds@yahoo.com"}}
+                 
+            />
         </div>
     );
 }
