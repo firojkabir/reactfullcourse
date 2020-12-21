235de6b0109d314983e592de8cca7f35a51a6177
Md Firoj Kabir <firoj.kabir65@gmail.com>
2020-12-21 13:23:23 +0100

    ToDo App phase 5

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -3,7 +3,11 @@ import React from 'react'
 function ToDoItem(props) {
     return (
         <div className="todo-item">
-            <input type="checkbox" checked={props.todos.completed}/>
+            <input 
+                type="checkbox" 
+                checked={props.todos.completed}
+                onChange={() => console.log("Changed!!")}    
+            />
             <p>{props.todos.text}</p>
         </div>
     )
